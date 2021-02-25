62e338e8b2639a3ef15ff90c1175f1ea6bab32ac
Dinesh <dineshkumar1deve@gmail.com>
2021-02-25 11:25:41 +0530

    Added Data Provider

--- src/test/java/com/qa/testcases/LoginPageTestOrange.java
+++ src/test/java/com/qa/testcases/LoginPageTestOrange.java
@@ -1,6 +1,7 @@
 package com.qa.testcases;
 
 import org.assertj.core.api.Assertions;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.BaseTest.app.BaseTest;
@@ -12,12 +13,21 @@ public final class LoginPageTestOrange extends BaseTest {
 
 	}
 
-	@Test
-	public void loginandlogoutTest() throws InterruptedException {
-		String title = new LoginPageOrange().EnterUsername("Admin").EnterPassword("admin123").loginbtn()
+	@Test(dataProvider = "Multiple login" )
+	public void loginandlogoutTest(String username , String password) throws InterruptedException {
+		String title = new LoginPageOrange().EnterUsername(username).EnterPassword(password).loginbtn()
 				.ClickWelcomebtn().logoutBtn().getTitle();
 		Assertions.assertThat(title).isEqualTo("OrangeHRM");
 
 	}
-
+	
+	
+	@DataProvider(name="Multiple login",parallel = true)
+	public Object[][] getData(){
+		return new Object[][] {
+			{"Admin","admin123"},
+			{"Admin","admin123"},
+			{"Admin","admin123"}
+		};
+	}
 }
